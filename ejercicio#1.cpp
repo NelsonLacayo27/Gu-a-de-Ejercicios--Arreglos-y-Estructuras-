2d692081a720b11e2d6095820f38292c0b440475
Nelson <130504241+NelsonLacayo27@users.noreply.github.com>
2023-04-24 23:05:47 -0600

    Update ejercicio#1.cpp

--- ejercicio#1.cpp
+++ ejercicio#1.cpp
@@ -1,11 +1,17 @@
-#include <iostream>
 
-int main() {
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
     char arreglo[] = {'0', '1', '5', '7', '-', 'M', 'P', '2', '0', '2', '3'};
-    
-    for (int i = 0; i < sizeof(arreglo)/sizeof(arreglo[0]); i++) {
-        std::cout << arreglo[i] << " ";
+
+    // Imprimir los valores almacenados en el arreglo con formato adecuado
+    for (int i = 0; i < sizeof(arreglo) / sizeof(char); i++)
+    {
+        cout << setw(2) << arreglo[i] << " "; // Usamos setw(2) para imprimir cada valor con 2 caracteres de ancho
     }
-    
+
     return 0;
 }
